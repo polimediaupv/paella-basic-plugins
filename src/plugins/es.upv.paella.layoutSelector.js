0657522a9053df2032850b6f82bc7b402d545a06
Fernando Serrano Carpena <ferserc1@gmail.com>
2021-03-03 12:58:10 +0000

    Layout selector plugin added

---
+++ src/plugins/es.upv.paella.layoutSelector.js
@@ -0,0 +1,48 @@
+import { MenuButtonPlugin, utils } from 'paella-core';
+
+import presentationMode from '../icons/presentation-mode.svg';
+
+function loadIconAsync(url) {
+	return new Promise((resolve,reject) => {
+		fetch(url)
+			.then((icon) => {
+				return icon.text()
+			})
+			.then(svg => {
+				resolve(svg);
+			})
+			.catch(err => reject(err));
+	})
+}
+
+export default class LayoutSelectorPlugin extends MenuButtonPlugin {
+	async load() {
+		this.icon = presentationMode;
+	}
+	
+	async getMenu() {
+		const contentSettings = this.player.videoContainer.validContentSettings;
+		return Promise.all(await contentSettings.map(async item => {
+			const configPath = utils.joinPath([this.player.configResourcesUrl, item.icon]);
+			const icon = await utils.loadSvgIcon(configPath)
+			return {
+				id: item.id,
+				title: item.title,
+				icon: icon,
+				selected: this.player.videoContainer.layoutId === item.id
+			}
+		}));
+	}
+	
+	get showTitles() {
+		return false;
+	}
+	
+	get buttonType() {
+		return "radio";
+	}
+	
+	itemSelected(itemData, menuItems) {
+		this.player.videoContainer.setLayout(itemData.id);
+	}
+}
