7bf8ea754eeb40a97bc0194bc2bbb4f0b685210b
Fernando Serrano Carpena <ferserc1@gmail.com>
2021-05-20 11:07:40 +0200

    Captions selector plugin added

--- src/plugins/es.upv.paella.captionsSelectorPlugin.js
+++ src/plugins/es.upv.paella.captionsSelectorPlugin.js
@@ -1,26 +1,53 @@
-import { MenuButtonPlugin } from 'paella-core';
+import { MenuButtonPlugin, Events, bindEvent } from 'paella-core';
 
 // TODO: Load captions icon
 import captionsPlugin from '../icons/screen.svg';
 
 export default class CaptionsSelectorPlugin extends MenuButtonPlugin{
-    async isEnabled() {
-        const enabled = super.isEnabled();
-        if (enabled) {
-            const player = this.player;
-            console.log(player);
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
     async load() {
         this.icon = captionsPlugin;
+        this._captionsCanvas = await this.player.captionsCanvas;
+
+        if (this._captionsCanvas.captions.length==0) {
+            this.hide();
+        }
+
+        bindEvent(this.player, Events.CAPTIONS_CHANGED, () => {
+            if (this._captionsCanvas.captions.length>0) {
+                this.show();
+            }
+        });
     }
 
     async getMenu() {
-        return [];
+        const result = [
+            {
+                id: -1,
+                title: "Disabled",
+                index: -1
+            }
+        ];
+
+        this._captionsCanvas.captions.forEach((c,i) => {
+            result.push({
+                id: c.language,
+                title: c.label,
+                index: i
+            });
+        })
+        return result;
+    }
+
+    get buttonType() {
+        return "radio";
+    }
+
+    itemSelected(itemData) {
+        if (itemData.index === -1) {
+            this._captionsCanvas.disableCaptions();
+        }
+        else {
+            this._captionsCanvas.enableCaptions({ index: itemData.index });
+        }
     }
 }
