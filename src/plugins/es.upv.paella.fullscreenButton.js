9864e263758f8189a884455ffe5201b9684333f2
Fernando Serrano Carpena <ferserc1@gmail.com>
2024-02-21 11:55:38 +0100

    FullScreen plugin updated to support fallback method in iOS devices

--- src/plugins/es.upv.paella.fullscreenButton.js
+++ src/plugins/es.upv.paella.fullscreenButton.js
@@ -1,8 +1,7 @@
 import { Events, bindEvent, ButtonPlugin } from 'paella-core';
 import BasicPluginsModule from './BasicPluginsModule';
 
-import fullscreenIcon from '../icons/fullscreen.svg';
-import windowedIcon from '../icons/windowed.svg';
+import { FullscreenIcon, WindowedIcon } from '../icons/fullscreen-icons.js';
 
 export default class PauseButtonPlugin extends ButtonPlugin {
 	getPluginModuleInstance() {
@@ -21,14 +20,20 @@ export default class PauseButtonPlugin extends ButtonPlugin {
         return this.getAriaLabel();
     }
 	
+	get isFallbackFSAvailable() {
+		const { width: viewportWidth, height: viewportHeight } = globalThis.visualViewport;
+		const { w: playerWidth, h: playerHeight } = this.player.containerSize;
+		return viewportWidth !== playerWidth || viewportHeight !== playerHeight;
+	}
+
 	async isEnabled() {
-		const enabled = await super.isEnabled()
-		return enabled && this.player.isFullScreenSupported()
+		const enabled = await super.isEnabled();
+		return enabled && this.player.isFullScreenSupported() || this.isFallbackFSAvailable;
 	}
 	
 	async load() {
-		const fsIcon = this.player.getCustomPluginIcon(this.name,"fullscreenIcon") || fullscreenIcon;
-		const wIcon = this.player.getCustomPluginIcon(this.name,"windowedIcon") || fullscreenIcon;
+		const fsIcon = this.player.getCustomPluginIcon(this.name,"fullscreenIcon") || FullscreenIcon;
+		const wIcon = this.player.getCustomPluginIcon(this.name,"windowedIcon") || WindowedIcon;
 		this.icon = fsIcon
 		bindEvent(this.player, Events.FULLSCREEN_CHANGED, (data) => {
 			if (data.status) {
@@ -40,12 +45,33 @@ export default class PauseButtonPlugin extends ButtonPlugin {
 		})
 	}
 	
-	async action() {
+	async toggleFS() {
 		if (this.player.isFullscreen) {
 			await this.player.exitFullscreen();
 		}
 		else {
 			await this.player.enterFullscreen();
+		}
+	}
+
+	toggleFallbackFS() {
+		if (this.player.containerElement.classList.contains("paella-fallback-fullscreen")) {
+			this.player.containerElement.classList.remove("paella-fallback-fullscreen");
+		}
+		else {
+			this.player.containerElement.classList.add("paella-fallback-fullscreen");
+		}
+		setTimeout(() => {
+			this.player.resize();
+		}, 100);
+	}
+	
+	async action() {
+		if (this.player.isFullScreenSupported()) {
+			await this.toggleFS();
+		}
+		else {
+			this.toggleFallbackFS();
 		}
 	}
 }
